53ba8aabfd8e11543023a6206b2e162106095989
kaushik <83204057+kaushiksheel@users.noreply.github.com>
2024-03-22 19:49:45 +0530

    minor change

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/:path*","/"],
-  ignoredRoutes: ["/api/products","/"],
+  ignoredRoutes: ["/api/products","/api"],
 });
 
 export const config = {
